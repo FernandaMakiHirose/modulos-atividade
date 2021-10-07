b9cc2c99edc455ccaa0c05366eb4c76b681150a9
Fernanda Maki Hirose <72028645+FernandaMakiHirose@users.noreply.github.com>
2021-10-07 15:00:13 -0300

    Add files via upload

---
+++ main.mjs
@@ -0,0 +1,7 @@
+import { mostraIdade, mostraCidade, mostraHobby } from './funcoes.mjs';
+
+console.log(mostraIdade('Fernanda Maki', 18));
+
+console.log(mostraCidade('Hirose', 'São Paulo'));
+
+console.log(mostraHobby('Assistir séries', 'Ouvir música'));
